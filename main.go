69a449934af550d55b797b9df88fba7d6378a2a1
Charlie Haley <27928201+charlie-haley@users.noreply.github.com>
2021-02-13 16:02:47 +0000

    Loop over values, add sleep to goroutine

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -24,22 +25,30 @@ var (
 func main() {
 	conn, _ := telnet.DialTo(fmt.Sprintf("%s:%v", os.Getenv("CISCO_IP"), os.Getenv("CISCO_PORT")))
 
-	conn.Write([]byte(os.Getenv("CISCO_PASS") + "\r\n"))
-	read(conn)
-
-	conn.Write([]byte("show env all\r\n"))
-	response := read(conn)
-
-	r := regexp.MustCompile("Temperature Value: .*?Celsius*")
-	match := r.FindString(response)
-	s := strings.Split(match, " ")[2]
-	fmt.Println(s)
-
-	temp, _ := strconv.ParseFloat(s, 8)
-	telnetTemp.WithLabelValues(os.Getenv("CISCO_IP")).Set(temp)
+	go getData(conn)
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":9504", nil)
+}
+
+func getData(conn *telnet.Conn) {
+	for {
+		conn.Write([]byte(os.Getenv("CISCO_PASS") + "\r\n"))
+		read(conn)
+
+		conn.Write([]byte("show env all\r\n"))
+		response := read(conn)
+
+		r := regexp.MustCompile("Temperature Value: .*?Celsius*")
+		match := r.FindString(response)
+		s := strings.Split(match, " ")[2]
+		fmt.Println(s)
+
+		temp, _ := strconv.ParseFloat(s, 8)
+		telnetTemp.WithLabelValues(os.Getenv("CISCO_IP")).Set(temp)
+
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func read(conn *telnet.Conn) string {
